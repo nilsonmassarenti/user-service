a6f187b365a2266372214df8665aa885dee78c4c
Nilson Massarenti <nilsonmassarenti@gmail.com>
2017-09-15 03:27:01 -0300

    Delete erros

--- user-service/src/main/java/com/nilsonmassarenti/app/userservice/entity/Phone.java
+++ user-service/src/main/java/com/nilsonmassarenti/app/userservice/entity/Phone.java
@@ -1,12 +1,8 @@
 package com.nilsonmassarenti.app.userservice.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
-import org.hibernate.annotations.GenericGenerator;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
